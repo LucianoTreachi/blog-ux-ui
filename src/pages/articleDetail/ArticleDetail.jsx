81b31a8e596862b4fca10cccb6135d4d0a5b74ff
lucianotreachi <lucianotreachi@outlook.com>
2025-08-04 09:24:31 -0300

    Implementar Helmet en cada página para agregar metadatos

--- src/pages/articleDetail/ArticleDetail.jsx
+++ src/pages/articleDetail/ArticleDetail.jsx
@@ -1,3 +1,4 @@
+import { Helmet } from "react-helmet";
 import { ImCross } from "react-icons/im";
 import { FaCheck } from "react-icons/fa";
 import { useParams } from "react-router-dom";
@@ -11,45 +12,51 @@ export default function ArticleDetail() {
   const articleSelected = data.find((article) => article.url === url);
 
   return (
-    <main>
-      <section className={styles.section}>
-        <NavigateToTop />
-        <div className={styles.container}>
-          <GoBackButton />
-          <h1 className={styles.title}>{articleSelected.title}</h1>
-          <span className={styles.time}>{articleSelected.time}</span>
-          <img
-            className={styles.cover}
-            src={articleSelected.cover}
-            alt=""
-            width={1000}
-            height={600}
-          />
-          <div className={styles.description}>
-            {articleSelected.description}
+    <>
+      <Helmet>
+        <title>{articleSelected.title} | Artículo</title>
+      </Helmet>
+
+      <main>
+        <section className={styles.section}>
+          <NavigateToTop />
+          <div className={styles.container}>
+            <GoBackButton />
+            <h1 className={styles.title}>{articleSelected.title}</h1>
+            <span className={styles.time}>{articleSelected.time}</span>
+            <img
+              className={styles.cover}
+              src={articleSelected.cover}
+              alt=""
+              width={1000}
+              height={600}
+            />
+            <div className={styles.description}>
+              {articleSelected.description}
+            </div>
+            <h2 className={styles.redTitle}>
+              <ImCross className={styles.crossIcon} /> Evita esto
+            </h2>
+            <img
+              className={styles.image}
+              src={articleSelected.image1}
+              alt="Imagen ilustrativa 1"
+              width={1920}
+              height={1920}
+            />
+            <h2 className={styles.greenTitle}>
+              <FaCheck className={styles.checkIcon} /> Mejor esto
+            </h2>
+            <img
+              className={styles.image}
+              src={articleSelected.image2}
+              alt="Imagen ilustrativa 2"
+              width={1920}
+              height={1920}
+            />
           </div>
-          <h2 className={styles.redTitle}>
-            <ImCross className={styles.crossIcon} /> Evita esto
-          </h2>
-          <img
-            className={styles.image}
-            src={articleSelected.image1}
-            alt="Imagen ilustrativa 1"
-            width={1920}
-            height={1920}
-          />
-          <h2 className={styles.greenTitle}>
-            <FaCheck className={styles.checkIcon} /> Mejor esto
-          </h2>
-          <img
-            className={styles.image}
-            src={articleSelected.image2}
-            alt="Imagen ilustrativa 2"
-            width={1920}
-            height={1920}
-          />
-        </div>
-      </section>
-    </main>
+        </section>
+      </main>
+    </>
   );
 }
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,20 @@
+import { Helmet } from "react-helmet";
 import Hero from "../../sections/hero/Hero";
 import ArticleList from "../../sections/articleList/ArticleList";
 
 export default function Home() {
   return (
-    <main>
-      <Hero />
-      <ArticleList />
-    </main>
+    <>
+      <Helmet>
+        <title>
+          Estrategias UX/UI para un diseño atractivo | Por Luciano Treachi
+        </title>
+      </Helmet>
+
+      <main>
+        <Hero />
+        <ArticleList />
+      </main>
+    </>
   );
 }
